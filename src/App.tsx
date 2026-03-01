5d5b0e7d61b0163301341bd7630439d7402e86cc
anmolblogging <anmolblogging@gmail.com>
2026-03-01 16:41:59 +0000

    react router update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Suspense, lazy ,useEffect , useLocation} from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy ,useEffect } from "react";
+import { BrowserRouter, Routes, Route ,useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Chatbot from "./components/Chatbot";
